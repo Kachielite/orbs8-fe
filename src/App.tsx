a62a99344fa995e3140c242da191cefd3b7d9133
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-09-24 04:57:52 +0300

    feat: update App component and styles with Tailwind CSS integration and new theme variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { useState } from 'react'
-
 export default function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div style={{ padding: 24 }}>
-      <h1>Vite + React + TypeScript</h1>
-      <p>Alias test: importing from @/ works!</p>
-      <button onClick={() => setCount((c) => c + 1)}>count is {count}</button>
+    <div>
+        <p className="text-5xl text-red-600">Orbs8</p>
     </div>
   )
 }
