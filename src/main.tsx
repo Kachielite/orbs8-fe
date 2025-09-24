87bb8f629f3f8dcd50aac733b2f8d26e14a1400a
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-09-24 06:30:51 +0300

    feat: configure dependency injection for authentication use cases and data sources

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App'
 import '@/index.css'
+import {configureAuthContainer} from "@/core/init-dependencies/auth.dependency";
+
+// Initialize dependencies
+configureAuthContainer()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
