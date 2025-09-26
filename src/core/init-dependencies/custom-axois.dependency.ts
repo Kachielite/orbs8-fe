9727313720ac1c04d43b74fd0d8ed135ebd1f758
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-09-26 07:12:44 +0300

    feat: remove unused custom Axios instance from dependency configuration

--- src/core/init-dependencies/custom-axois.dependency.ts
+++ src/core/init-dependencies/custom-axois.dependency.ts
@@ -4,8 +4,3 @@ import AxiosClient from "../network/custom-axios";
 export function configureCustomAxiosContainer(){
     container.registerSingleton("AxiosClient", AxiosClient);
 }
-
-const axiosClient = container.resolve<AxiosClient>("AxiosClient");
-const customAxiosInstance = axiosClient.getInstance();
-
-export default customAxiosInstance;
