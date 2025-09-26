d468bb8fc492b878eb79a798713c1b753304a050
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-09-26 07:24:27 +0300

    feat: implement user slice with actions and initial state for Zustand store

--- src/core/common/presentation/state/store/index.ts
+++ src/core/common/presentation/state/store/index.ts
@@ -1,15 +1,18 @@
-import { persist } from 'zustand/middleware';
-import { create } from 'zustand/react';
+import {persist} from 'zustand/middleware';
+import {create} from 'zustand/react';
 
-import { createAuthSlice } from '@/features/authentication/presentation/state/store/slice';
-import { AuthSlice } from '@/features/authentication/presentation/state/store/types';
+import {createAuthSlice} from '@/features/authentication/presentation/state/store/slice';
+import {AuthSlice} from '@/features/authentication/presentation/state/store/types';
+import {UserSlice} from "@/features/user/presentation/state/store/types";
+import {createUserSlice} from "@/features/user/presentation/state/store/slice";
 
-type AppState = AuthSlice;
+type AppState = AuthSlice & UserSlice;
 
 export const useAppStore = create<AppState>()(
   persist(
     (...a) => ({
       ...createAuthSlice(...a),
+        ...(createUserSlice(...a))
     }),
     {
       name: 'auth-data', // unique name
