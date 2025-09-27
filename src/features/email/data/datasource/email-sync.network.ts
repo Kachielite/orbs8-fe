83e81280cf1fa583f765d99cc2fafd1632b6fc82
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-09-27 07:40:55 +0300

    feat: implement EmailSyncNetwork for email synchronization API interactions

---
+++ src/features/email/data/datasource/email-sync.network.ts
@@ -0,0 +1,53 @@
+import {inject, injectable} from "tsyringe";
+import CustomAxios from "@/core/network/custom-axios";
+import extractErrorNetwork from "@/core/helpers/extract-error-network";
+import {GetOauthTokenSchemaType} from "@/features/email/presentation/validation/email-sync";
+
+@injectable()
+export class EmailSyncNetwork {
+    private readonly emailPath = '/email';
+
+    constructor(
+        @inject(CustomAxios) private readonly axios: CustomAxios
+    ) {}
+
+    public async getOAuthUrl() {
+        try {
+            const url = `${this.emailPath}/get-auth`
+            const response = await this.axios.getInstance().get(url);
+            return response.data;
+        } catch (error) {
+            throw extractErrorNetwork(error, 'UserNetwork:getOAuthUrl');
+        }
+    }
+
+    public async getToken(request: GetOauthTokenSchemaType){
+        try {
+            const url = `${this.emailPath}/get-token`
+            const response = await this.axios.getInstance().post(url, request);
+            return response.data;
+        } catch (error) {
+            throw extractErrorNetwork(error, 'UserNetwork:getToken');
+        }
+    }
+
+    public async getSyncStatus(){
+        try {
+            const url = `${this.emailPath}/sync-status`
+            const response = await this.axios.getInstance().get(url);
+            return response.data;
+        } catch (error) {
+            throw extractErrorNetwork(error, 'UserNetwork:getSyncStatus');
+        }
+    }
+
+    public async syncEmail(){
+        try {
+            const url = `${this.emailPath}/manual-sync`
+            const response = await this.axios.getInstance().post(url);
+            return response.data;
+        } catch (error) {
+            throw extractErrorNetwork(error, 'UserNetwork:syncEmail');
+        }
+    }
+}
