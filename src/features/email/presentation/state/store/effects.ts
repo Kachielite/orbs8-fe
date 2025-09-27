130693304a8663d2ca7586b1c817ffebb533b84f
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-09-27 16:42:56 +0300

    feat: add effects for OAuth URL retrieval, sync status, email synchronization, and token retrieval

---
+++ src/features/email/presentation/state/store/effects.ts
@@ -0,0 +1,66 @@
+import { getEmailSyncUseCases } from '@/core/init-dependencies/email-sync.dependency';
+import { NoParams } from '@/core/use-case';
+import { Failure } from '@/core/errors/failure.error';
+import { fold } from 'fp-ts/Either';
+import { EmailSyncStatusEntity } from '@/features/email/domain/entity/email-sync-status.entity';
+import { GetOauthTokenSchemaType } from '@/features/email/presentation/validation/email-sync';
+import { GetTokenUseCaseParam } from '@/features/email/domain/use-case/get-token';
+
+export const getOAuthUrlEffect = async () => {
+  const response = await getEmailSyncUseCases().getOauthUrlUseCase.execute(
+    new NoParams()
+  );
+
+  return fold<Failure, string, string>(
+    failure => {
+      throw failure;
+    },
+    url => {
+      return url;
+    }
+  )(response);
+};
+
+export const getSyncStatusEffect = async () => {
+  const response = await getEmailSyncUseCases().getSyncStatusUseCase.execute(
+    new NoParams()
+  );
+
+  return fold<Failure, EmailSyncStatusEntity, EmailSyncStatusEntity>(
+    failure => {
+      throw failure;
+    },
+    emailSyncStatus => {
+      return emailSyncStatus;
+    }
+  )(response);
+};
+
+export const getSyncEmailEffect = async () => {
+  const response = await getEmailSyncUseCases().syncEmailUseCase.execute(
+    new NoParams()
+  );
+
+  return fold<Failure, string, string>(
+    failure => {
+      throw failure;
+    },
+    message => {
+      return message;
+    }
+  )(response);
+};
+
+export const getTokenEffect = async (request: GetOauthTokenSchemaType) => {
+  const response = await getEmailSyncUseCases().getTokenUseCase.execute(
+    new GetTokenUseCaseParam(request)
+  );
+  return fold<Failure, string, string>(
+    failure => {
+      throw failure;
+    },
+    message => {
+      return message;
+    }
+  )(response);
+};
