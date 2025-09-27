4864f166a776eb56f95e0773611312c8a9caf1b5
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-09-27 08:09:09 +0300

    feat: add email synchronization dependency configuration and use cases

--- src/features/email/domain/use-case/sync-email.ts
+++ src/features/email/domain/use-case/sync-email.ts
@@ -1,11 +1,11 @@
-import { Failure } from '@/core/errors/failure.error';
-import { inject, injectable } from 'tsyringe';
-import { NoParams, UseCase } from '@/core/use-case';
-import { type IEmailSyncRepository } from '@/features/email/domain/repository/email-sync.repository';
-import { Either } from 'fp-ts/lib/Either';
+import {Failure} from '@/core/errors/failure.error';
+import {inject, injectable} from 'tsyringe';
+import {NoParams, UseCase} from '@/core/use-case';
+import {type IEmailSyncRepository} from '@/features/email/domain/repository/email-sync.repository';
+import {Either} from 'fp-ts/lib/Either';
 
 @injectable()
-export class SyncEmail implements UseCase<string, NoParams> {
+export class SyncEmailUseCase implements UseCase<string, NoParams> {
   constructor(
     @inject('IEmailSyncRepository')
     private readonly emailSyncRepository: IEmailSyncRepository
