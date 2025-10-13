762c11cee7937a580488b8e2564072117609e94c
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-10-13 19:43:14 +0300

    feat: add Skeleton, Separator, Collapsible, Avatar, Tooltip components and update Dashboard layout with new sidebar structure

--- src/core/common/presentation/components/layouts/dashboard.layout.tsx
+++ src/core/common/presentation/components/layouts/dashboard.layout.tsx
@@ -1,10 +1,52 @@
-import { Outlet } from 'react-router-dom';
+import {AppSidebar} from "@/core/common/presentation/components/app-sidebar"
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/core/common/presentation/components/ui/breadcrumb"
+import {Separator} from "@/core/common/presentation/components/ui/separator"
+import {SidebarInset, SidebarProvider, SidebarTrigger,} from "@/core/common/presentation/components/ui/sidebar"
 
 const DashboardLayout = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center w-screen">
-      <Outlet />
-    </div>
+   <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-[orientation=vertical]:h-4"
+            />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="bg-muted/50 aspect-video rounded-xl" />
+            <div className="bg-muted/50 aspect-video rounded-xl" />
+            <div className="bg-muted/50 aspect-video rounded-xl" />
+          </div>
+          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 };
 
--- src/features/dashboard/presentation/pages/dashboard.page.tsx
+++ src/features/dashboard/presentation/pages/dashboard.page.tsx
@@ -3,7 +3,6 @@ import {Navigate} from 'react-router-dom';
 
 import {GlobalLoader} from '@/core/common/presentation/components/global-loader';
 import {useAppStore} from '@/core/common/presentation/state/store';
-import SyncEmailAlert from "@/features/email/presentation/components/sync-email-alert";
 import useGetUser from '@/features/user/presentation/state/hook/use-get-user';
 
 function DashboardPage() {
@@ -18,7 +17,7 @@ function DashboardPage() {
     return <Navigate to="/link-email" replace />;
   }
 
-  return <div><SyncEmailAlert/></div>;
+  return <div>dashboard</div>;
 }
 
 export default DashboardPage;
