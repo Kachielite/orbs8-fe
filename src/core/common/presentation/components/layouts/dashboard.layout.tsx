47b8c6b21f47964e05805c78cb62d454c441e078
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-10-14 05:18:50 +0300

    feat: implement SideHeader component and add mode toggle functionality

--- src/core/common/presentation/components/layouts/dashboard.layout.tsx
+++ src/core/common/presentation/components/layouts/dashboard.layout.tsx
@@ -1,41 +1,11 @@
-import { useLocation } from 'react-router-dom';
-
-import { AppSidebar } from '@/core/common/presentation/components/app-sidebar';
-import { GlobalLoader } from '@/core/common/presentation/components/global-loader';
+import {AppSidebar} from '@/core/common/presentation/components/app-sidebar';
+import {GlobalLoader} from '@/core/common/presentation/components/global-loader';
 import PageLayout from '@/core/common/presentation/components/layouts/page.layout';
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbList,
-  BreadcrumbPage,
-} from '@/core/common/presentation/components/ui/breadcrumb';
-import { Separator } from '@/core/common/presentation/components/ui/separator';
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from '@/core/common/presentation/components/ui/sidebar';
+import SideHeader from "@/core/common/presentation/components/side-header";
+import {SidebarInset, SidebarProvider,} from '@/core/common/presentation/components/ui/sidebar';
 import useGetUser from '@/features/user/presentation/state/hook/use-get-user';
 
 const DashboardLayout = () => {
-  const location = useLocation();
-  const { pathname } = location;
-  const getPageName = () => {
-    switch (pathname) {
-      case '/':
-        return 'Dashboard';
-      case '/transactions':
-        return 'Transactions';
-      case '/accounts':
-        return 'Accounts';
-      case '/insights':
-        return 'Insights';
-      case '/settings':
-        return 'Settings';
-      default:
-        return 'Dashboard';
-    }
-  };
 
   const { isFetchingUser } = useGetUser();
   if (isFetchingUser) {
@@ -46,22 +16,7 @@ const DashboardLayout = () => {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator
-              orientation="vertical"
-              className="mr-2 data-[orientation=vertical]:h-4"
-            />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem>
-                  <BreadcrumbPage>{getPageName()}</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
+          <SideHeader/>
         <PageLayout />
       </SidebarInset>
     </SidebarProvider>
