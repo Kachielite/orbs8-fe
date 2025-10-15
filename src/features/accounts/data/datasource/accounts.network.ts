8a27f9a44f210d77ae78abfa73cf56d2bda28ad3
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-10-15 10:01:02 +0300

    feat(accounts): implement AccountsNetwork class for account-related API interactions

---
+++ src/features/accounts/data/datasource/accounts.network.ts
@@ -0,0 +1,39 @@
+import {inject, injectable} from "tsyringe";
+
+import {BASE_URL} from "@/core/constants/env.constants";
+import extractErrorNetwork from "@/core/helpers/extract-error-network";
+import CustomAxios from "@/core/network/custom-axios";
+
+@injectable()
+export class AccountsNetwork {
+    private readonly accountPath = `${BASE_URL}/account`;
+
+    constructor(@inject(CustomAxios) private readonly axios: CustomAxios) {}
+
+    public async getAccountById(id: number) {
+        try {
+            const response = await this.axios.getInstance().get(`${this.accountPath}/${id}`);
+            return response.data;
+        } catch (error) {
+            throw extractErrorNetwork(error, 'AccountsNetwork:getAccountById');
+        }
+    }
+
+    public async getAccounts() {
+        try {
+            const response = await this.axios.getInstance().get(`${this.accountPath}`);
+            return response.data;
+        } catch (error) {
+            throw extractErrorNetwork(error, 'AccountsNetwork:getAccounts');
+        }
+    }
+
+    public async getAccountSummary() {
+        try {
+            const response = await this.axios.getInstance().get(`${this.accountPath}/summary`);
+            return response.data;
+        } catch (error) {
+            throw extractErrorNetwork(error, 'AccountsNetwork:getAccountSummary');
+        }
+    }
+}
