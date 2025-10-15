a895de0d123be2a911826843ef86d29df345abc4
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-10-15 17:14:43 +0300

    feat(accounts): add ExchangeRate component to display currency exchange rates and integrate it into AccountsPage

--- src/features/accounts/presentation/pages/accounts.page.tsx
+++ src/features/accounts/presentation/pages/accounts.page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import AccountsCards from '@/features/accounts/presentation/components/accounts.cards';
+import ExchangeRate from "@/features/accounts/presentation/components/exchange-rate";
 
 function AccountsPage() {
   return (
     <div className="w-full flex flex-col gap-4">
+        <ExchangeRate/>
       <AccountsCards />
     </div>
   );
