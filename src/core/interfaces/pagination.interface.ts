09d2b4cc2207706748c07ab897ac09be45d41e20
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-10-16 09:40:16 +0300

    feat(transactions): refactor pagination interface to class and implement transaction data source

--- src/core/interfaces/pagination.interface.ts
+++ src/core/interfaces/pagination.interface.ts
@@ -1,8 +1,24 @@
-export interface PaginationInterface<T> {
+export class Pagination<T> {
     data: T[];
     total: number;
     page: number;
     limit: number;
     hasNextPage: boolean;
     hasPreviousPage: boolean;
+
+    constructor(params: {
+        data: T[];
+        total: number;
+        page: number;
+        limit: number;
+        hasNextPage: boolean;
+        hasPreviousPage: boolean;
+    }) {
+        this.data = params.data;
+        this.total = params.total;
+        this.page = params.page;
+        this.limit = params.limit;
+        this.hasNextPage = params.hasNextPage;
+        this.hasPreviousPage = params.hasPreviousPage;
+    }
 }
