4c3f48bbada0d4aa6ec7945a97981ca4892efd45
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-10-20 09:43:58 +0300

    feat(notifications): implement NotificationRepository for managing notifications

---
+++ src/features/notification/domain/repository/notification.repository.ts
@@ -0,0 +1,14 @@
+import {Either} from 'fp-ts/Either';
+
+import {Failure} from '@/core/errors/failure.error';
+import {Pagination} from '@/core/interfaces/pagination.interface';
+import {INotificationQuery} from '@/features/notification/domain/entity/interface/notification.interface';
+import {NotificationEntity} from '@/features/notification/domain/entity/notification.entity';
+
+export interface INotificationRepository {
+    getNotification(id: number): Promise<Either<Failure, NotificationEntity>>;
+
+    getNotifications(query: INotificationQuery): Promise<Either<Failure, Pagination<NotificationEntity>>>;
+
+    markAsRead(id: number): Promise<Either<Failure, string>>;
+}
