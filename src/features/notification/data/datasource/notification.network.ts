d4e3449844f417b3abc07145069d1d5101732db4
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-10-20 09:29:45 +0300

    feat(notifications): implement NotificationNetwork service for fetching and managing notifications

---
+++ src/features/notification/data/datasource/notification.network.ts
@@ -0,0 +1,49 @@
+import {inject, injectable} from "tsyringe";
+
+import {BASE_URL} from "@/core/constants/env.constants";
+import extractErrorNetwork from "@/core/helpers/extract-error-network";
+import CustomAxios from "@/core/network/custom-axios";
+import {INotificationQuery} from "@/features/notification/domain/entity/interface/notification.interface";
+
+injectable()
+
+export class NotificationNetwork {
+    private readonly path = `${BASE_URL}/notification`;
+
+    constructor(@inject('AxiosClient') private readonly axios: CustomAxios) {
+    }
+
+    public async getNotification(id: number) {
+        try {
+            const response = await this.axios.getInstance().get(`${this.path}/${id}`);
+            return response.data;
+        } catch (error) {
+            throw extractErrorNetwork(error, 'NotificationNetwork:getNotification');
+        }
+    }
+
+    public async getNotifications(query: INotificationQuery) {
+        try {
+            let url = `${this.path}?page=${query.page}&limit=${query.limit}`;
+
+            if (query.isRead) {
+                url += `&isRead=${query.isRead}`
+            }
+            const response = await this.axios.getInstance().get(url, {
+                params: query
+            });
+            return response.data;
+        } catch (error) {
+            throw extractErrorNetwork(error, 'NotificationNetwork:getNotifications');
+        }
+    }
+
+    public async markAsRead(id: number) {
+        try {
+            const response = await this.axios.getInstance().put(`${this.path}/${id}`);
+            return response.data;
+        } catch (error) {
+            throw extractErrorNetwork(error, 'NotificationNetwork:markAsRead');
+        }
+    }
+}
