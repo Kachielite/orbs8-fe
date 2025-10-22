f3141f6e6e2d9877ef3a5650917869ddb05ad572
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-10-22 06:05:39 +0300

    feat(notifications): refactor SideHeader and update TransactionHeader styling for improved UI

--- src/core/common/presentation/components/side-header.tsx
+++ src/core/common/presentation/components/side-header.tsx
@@ -1,55 +1,16 @@
 import React from 'react';
-import {useLocation} from 'react-router-dom';
 
 import {ModeToggle} from '@/core/common/presentation/components/mode-toggle';
 import NotificationToggle from '@/core/common/presentation/components/notification-toggle';
-import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbList,
-    BreadcrumbPage,
-} from '@/core/common/presentation/components/ui/breadcrumb';
-import {Separator} from '@/core/common/presentation/components/ui/separator';
 import {SidebarTrigger} from '@/core/common/presentation/components/ui/sidebar';
 
 function SideHeader() {
-  const location = useLocation();
-  const { pathname } = location;
-  const getPageName = () => {
-    switch (pathname) {
-      case '/':
-        return 'Dashboard';
-      case '/transactions':
-        return 'Transactions';
-      case '/accounts':
-        return 'Accounts';
-      case '/insights':
-        return 'Insights';
-      case '/settings':
-        return 'Settings';
-      default:
-          return null;
-    }
-  };
 
   return (
     <header className="flex h-16 pt-2.5 shrink-0 items-start gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
       <div className="flex items-center gap-2 px-4 w-full">
         <div className="flex items-center gap-2">
           <SidebarTrigger className="-ml-1" />
-          <Separator
-            orientation="vertical"
-            className="mr-2 data-[orientation=vertical]:h-4"
-          />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem>
-                <BreadcrumbPage className="font-bold">
-                  {getPageName()}
-                </BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
         </div>
         <div className="ml-auto flex items-center gap-2">
           <NotificationToggle />
--- src/features/transactions/presentation/components/transaction-header.tsx
+++ src/features/transactions/presentation/components/transaction-header.tsx
@@ -8,8 +8,8 @@ function TransactionHeader() {
         <div>
             <div className="flex flex-col lg:flex-row gap-2 items-center justify-between mb-2">
                 <div className="flex items-center gap-3">
-                    <div className="p-2 bg-primary/10 rounded-lg">
-                        <ArrowRightLeft className="h-6 w-6"/>
+                    <div className="p-2 bg-primary rounded-lg">
+                        <ArrowRightLeft className="h-6 w-6 text-white"/>
                     </div>
                     <div>
                         <h1 className="text-3xl font-bold text-foreground">
