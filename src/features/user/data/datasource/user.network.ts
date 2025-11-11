4de6868403715914a9a13e822cafd09c206d462b
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-11-11 21:39:47 +0300

    feat(user): add user update and password update methods with validation schemas

--- src/features/user/data/datasource/user.network.ts
+++ src/features/user/data/datasource/user.network.ts
@@ -1,9 +1,12 @@
-import { inject, injectable } from 'tsyringe';
-import CustomAxios from '@/core/network/custom-axios';
+import {inject, injectable} from 'tsyringe';
+
 import extractErrorNetwork from '@/core/helpers/extract-error-network';
+import CustomAxios from '@/core/network/custom-axios';
+import {UpdatePasswordSchemaType, UpdateUserSchemaType} from "@/features/user/presentation/validation/user.validation";
 
 @injectable()
 export class UserNetwork {
+    private readonly authPath = '/auth';
   private readonly userPath = '/auth/me';
 
   constructor(@inject(CustomAxios) private readonly axios: CustomAxios) {}
@@ -16,4 +19,23 @@ export class UserNetwork {
       throw extractErrorNetwork(error, 'UserNetwork:getUser');
     }
   }
+
+    public async updateUser(request: UpdateUserSchemaType) {
+        try {
+            const response = await this.axios.getInstance().put(this.authPath, request);
+            return response.data;
+        } catch (error) {
+            throw extractErrorNetwork(error, 'UserNetwork:updateUser');
+        }
+    }
+
+    public async updatePassword(request: UpdatePasswordSchemaType) {
+        try {
+            const response = await this.axios.getInstance().put(this.authPath, request);
+            return response.data;
+        } catch (error) {
+            throw extractErrorNetwork(error, 'UserNetwork:updatePassword');
+        }
+    }
 }
+
