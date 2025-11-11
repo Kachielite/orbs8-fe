d5f5ddcd9386d96edc90649dc377614760b837c3
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-11-11 22:12:21 +0300

    feat(user): add hooks for updating user and password with validation

---
+++ src/features/user/presentation/state/hook/use-update-password.tsx
@@ -0,0 +1,40 @@
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useForm} from "react-hook-form";
+import {useMutation} from "react-query";
+import {toast} from "sonner";
+
+import {updatePasswordEffect} from "@/features/user/presentation/state/store/effects";
+import {updatePasswordSchema, UpdatePasswordSchemaType} from "@/features/user/presentation/validation/user.validation";
+
+
+const useUpdatePassword = () => {
+    const updatePasswordForm = useForm<UpdatePasswordSchemaType>({
+        resolver: zodResolver(updatePasswordSchema),
+        defaultValues: {
+            currentPassword: '',
+            newPassword: ''
+        }
+    });
+
+    const {isLoading: isUpdatingPassword, mutateAsync: updatePasswordHandler} = useMutation(
+        async (payload: UpdatePasswordSchemaType) => {
+            return updatePasswordEffect({
+                currentPassword: payload.currentPassword,
+                newPassword: payload.newPassword,
+            })
+        }, {
+            onSuccess: () => {
+                updatePasswordForm.reset()
+                toast.success('User updated successfully')
+            }
+        }
+    );
+
+    return {
+        updatePasswordForm,
+        isUpdatingPassword,
+        updatePasswordHandler
+    }
+}
+
+export default useUpdatePassword
---
+++ src/features/user/presentation/state/hook/use-update-user.tsx
@@ -0,0 +1,46 @@
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useForm} from "react-hook-form";
+import {useMutation, useQueryClient} from "react-query";
+import {toast} from "sonner";
+
+import {useAppStore} from "@/core/common/presentation/state/store";
+import {updateUserEffect} from "@/features/user/presentation/state/store/effects";
+import {updateUserSchema, UpdateUserSchemaType} from "@/features/user/presentation/validation/user.validation";
+
+
+const useUpdateUser = () => {
+    const {user} = useAppStore();
+    const queryClient = useQueryClient();
+
+
+    const updateUserForm = useForm<UpdateUserSchemaType>({
+        resolver: zodResolver(updateUserSchema),
+        defaultValues: {
+            name: user?.name,
+            currencyCode: user?.preferredCurrency
+        }
+    });
+
+    const {isLoading: isUpdatingUser, mutateAsync: updateUserHandler} = useMutation(
+        async (payload: UpdateUserSchemaType) => {
+            return updateUserEffect({
+                name: payload.name,
+                currencyCode: payload.currencyCode,
+            })
+        }, {
+            onSuccess: () => {
+                queryClient.invalidateQueries(['user'])
+                updateUserForm.reset()
+                toast.success('User updated successfully')
+            }
+        }
+    );
+
+    return {
+        updateUserForm,
+        isUpdatingUser,
+        updateUserHandler
+    }
+}
+
+export default useUpdateUser
