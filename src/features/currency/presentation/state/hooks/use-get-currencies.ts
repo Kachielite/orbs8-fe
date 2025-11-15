26cb00b1a37009def229653b316ec903537cef80
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-11-15 16:08:35 +0300

    feat(currency): add custom hook for fetching and managing currency data

---
+++ src/features/currency/presentation/state/hooks/use-get-currencies.ts
@@ -0,0 +1,26 @@
+import {useQuery} from "react-query";
+
+import {useAppStore} from "@/core/common/presentation/state/store";
+import {getCurrenciesEffect} from "@/features/currency/presentation/state/store/effects";
+
+const useGetCurrencies = () => {
+    const {setCurrencies} = useAppStore();
+
+    const {isLoading, isRefetching, isFetching} = useQuery(
+        'getCurrencies',
+        async () => {
+            return getCurrenciesEffect();
+        },
+        {
+            onSuccess: (data) => {
+                setCurrencies(data);
+            }
+        }
+    );
+
+    return {
+        isFetchingCurrencies: isLoading || isRefetching || isFetching,
+    }
+}
+
+export default useGetCurrencies;
