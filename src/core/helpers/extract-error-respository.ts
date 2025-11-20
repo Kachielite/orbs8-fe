df9f6dc47411d07a32678a54fd800c2cb07beb14
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-11-20 21:31:03 +0300

    fix(extract-error-repository): enhance error handling to include Failure instances

--- src/core/helpers/extract-error-respository.ts
+++ src/core/helpers/extract-error-respository.ts
@@ -1,15 +1,10 @@
-import { Failure } from '@/core/errors/failure.error';
-import { ServerException } from '@/core/errors/server.error';
+import {Failure} from '@/core/errors/failure.error';
+import {ServerException} from '@/core/errors/server.error';
 
 const extractErrorRepository = (error: unknown, handlerName: string) => {
   console.error(handlerName, error);
-  console.log(
-    'instance of error',
-    error instanceof ServerException,
-    (error as ServerException).message
-  );
   const errorMessage =
-    error instanceof ServerException
+      error instanceof ServerException || error instanceof Failure
       ? error.message
       : 'An unknown error occurred';
   return new Failure(errorMessage);
